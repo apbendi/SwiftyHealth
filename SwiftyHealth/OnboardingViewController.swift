0ddfb27a445f108249da9e8d178bf31787c65215
Ben DiFrancesco <ben.difrancesco@gmail.com>
2016-03-22 12:18:58 -0400

    Implement task view controller delegate for consent flow

--- SwiftyHealth/OnboardingViewController.swift
+++ SwiftyHealth/OnboardingViewController.swift
@@ -1,7 +1,7 @@
 import UIKit
 import CMHealth
 
-class OnboardingViewController: UIViewController {
+class OnboardingViewController: UIViewController, ORKTaskViewControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -9,7 +9,32 @@ class OnboardingViewController: UIViewController {
 
     @IBAction func consentButtonDidPress(sender: UIButton) {
         let consentVC = ORKTaskViewController(task: ConsentBuilder.consentTask(), taskRunUUID: nil)
+        consentVC.delegate = self
 
-        self.presentViewController(consentVC, animated:true, completion:nil)
+        presentViewController(consentVC, animated:true, completion:nil)
+    }
+
+    // MARK: ORKTaskViewControllerDelegate
+
+    func taskViewController(taskViewController: ORKTaskViewController, didFinishWithReason reason: ORKTaskViewControllerFinishReason, error: NSError?) {
+        if let error = error {
+            print("Consent Error: \(error.localizedDescription)")
+            return
+        }
+
+        dismissViewControllerAnimated(true, completion: nil)
+
+        switch reason {
+        case .Completed:
+            handleConsentCompletion()
+        default:
+            print("Consent Dismissed")
+        }
+    }
+
+    // MARK: Private helpers
+
+    private func handleConsentCompletion() {
+        print("Handle Consent")
     }
 }
